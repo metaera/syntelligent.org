c8153826a01fa6fed6be6e01da0a34cba45f0add
wildone <wildone@users.noreply.github.com>
2020-09-28 10:29:45 +0000

    deploy: fc80473f6e54f7d93e3a161c2d2b56052d212afb

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Synthetic Intelligence Association",
+        "excerpt":"Welcome to Synthetic Intelligence Association! Synthetic Intelligence (SI), also called Artificial General Intelligence, is the next evolutionary step beyond current Artificial Intelligence, which includes Machine Learning, Deep Learning, Reinforcement Learning, etc. We believe that once achieved in a fully commercial form, this new technology promises to revolutionise the world as...","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "https://syntelligent.org/blog/2020/09/27/synthetic-intelligence-association",
+        "teaser": null
+      }]
