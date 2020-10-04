22d2523771ab66b58c4e635082c3288649eef6ca
Max Barrass <max.barrass@gmail.com>
2020-10-04 20:05:04 +1100

    update scripts

--- serve.ps1
+++ serve.ps1
@@ -10,5 +10,5 @@ if ($DOCKER_INSPECT_CHECK -contains "running") {
   docker exec -it $CONATINER_NAME bash
 } else {
   Write-Host "Starting Docker container to run build server:"
-  docker run --name $CONATINER_NAME -it --rm -p 8101:8100 -p 35729:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+  docker run --name $CONATINER_NAME -it --rm -p 8101:8100 -p 35728:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
 }
